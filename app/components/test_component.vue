7c6dd6f3ab19f1236c5f5716501d14ad510a8b62
nemnem202 <naimelhabbas@gmail.com>
2025-09-29 17:33:01 +0200

    test

---
+++ app/components/test_component.vue
@@ -0,0 +1,7 @@
+<template>
+  <button @click="increment_counter">{{ counter }}</button>
+</template>
+
+<script setup lang="ts">
+import { counter, increment_counter } from "~/scripts/test";
+</script>
